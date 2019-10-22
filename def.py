b8c362e67927b3d8b8f1adc4af98b68ff70b0d06
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-10-22 12:56:35 +0530

    Add files via upload

---
+++ def.py
@@ -0,0 +1,17 @@
+q = pow(3.3,4)
+print(q)
+def hello():
+    print('hello world !')
+hello()
+z = {'a':123,'b':456}
+print(z['b'],z['a'])
+from array import *
+my_array = array('i',[1,2,3,4,5,6,7,])
+for i in my_array:
+    print(i)
+
+
+
+    print(my_array.index(5))
+
+hello()
